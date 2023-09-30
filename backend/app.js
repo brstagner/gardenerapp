35916a82b550537955075b1c8b5a1167e2287a60
bryan <brstagner@gmail.com>
2023-09-30 18:50:37 -0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { NotFoundError } = require("./expressError");
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://gardenbloom.surge.sh");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
